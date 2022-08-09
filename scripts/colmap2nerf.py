0cc55242c4baf92c5f0c84e4c720dbb597ae2185
Realcat <realcat@126.com>
2022-08-09 14:47:42 +0800

    Update colmap2nerf.py

--- scripts/colmap2nerf.py
+++ scripts/colmap2nerf.py
@@ -238,6 +238,10 @@ if __name__ == "__main__":
         up = np.zeros(3)
         for line in f:
             line = line.strip()
+			# images with no 3D points
+			if len(line) == 0:
+				i = i + 1
+				continue
             if line[0] == "#":
                 continue
             i = i + 1
